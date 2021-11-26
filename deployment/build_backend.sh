1476adbbca3fc897ac80b9dc70c7f64eb53e3f38
Aleksandars991 <alexcsasha91@gmail.com>
2021-11-26 23:29:36 +0100

    - Fix build script.

--- deployment/build_backend.sh
+++ deployment/build_backend.sh
@@ -2,9 +2,11 @@
 
 echo "Building artifacts ..."
 
-for i in */backend/;
-     do
-          cd $i;
-          eval "npm install --production" || exit 1;
-          cd ..
-     done
+cd backend/
+
+for i in */ ; do
+     echo "$i"
+     cd $i;
+     eval "npm install --production" || exit 1;
+     cd ..
+done
