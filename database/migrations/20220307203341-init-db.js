4081f87871866bbe180a54b6e5fc031fa905e5e8
Aleksandars991 <alexcsasha91@gmail.com>
2022-03-09 00:37:38 +0100

    - Add outputs for aurora cluster and aurora secret.

--- database/migrations/20220307203341-init-db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  up: (queryInterface) => {
-    const { migrate } = require('./utils/index');
-    return migrate(__filename, queryInterface);
-  },
-  down: () => Promise.reject(new Error('error'))
-};
